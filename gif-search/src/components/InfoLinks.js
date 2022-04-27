ae7189b3f586c19ecf85a8ab23c0982a13aa186c
RosaNM <rosanmn.n@gmail.com>
2022-04-27 11:48:41 +0200

    minor fix

--- gif-search/src/components/InfoLinks.js
+++ gif-search/src/components/InfoLinks.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export default function InfoLinks() {
-    return (
-        <div className='links-div'>
-            <a class="fab fa-linkedin-in" href="www.linkedin.com/in/rosa-narvaez-munoz" target="_blank">LinkedIn</a>
+  return (
+    <div className="links-div">
+      <a
+        className="fab fa-linkedin-in"
+        href="www.linkedin.com/in/rosa-narvaez-munoz"
+        target="_blank"
+      >
+        LinkedIn
+      </a>
 
-            <a class="fab fa-github" href="https://github.com/RosaNarMu" target="_blank">GitHub</a>
-        </div>
-    )
+      <a
+        className="fab fa-github"
+        href="https://github.com/RosaNarMu"
+        target="_blank"
+      >
+        GitHub
+      </a>
+    </div>
+  );
 }
